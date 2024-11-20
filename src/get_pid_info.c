354f123b74c7467c26b921833e8ce0eb2cf63dfc
Jolan Rathelot <jrathelo@student.42nice.fr>
2024-11-20 16:56:35 +0100

    WORKING

--- src/get_pid_info.c
+++ src/get_pid_info.c
@@ -15,10 +15,10 @@
 
 struct pid_info {
     int pid;
-    u32 state;
+    unsigned int state;
     const void *stack;
-    u64 age;
-    // int * children;
+    unsigned long long age;
+    int *children;
     int parent_pid;
     char root_path[PATH_MAX];
     char working_directory[PATH_MAX];
@@ -28,11 +28,26 @@ static long    __sys_get_pid_info(struct pid_info __user * p_info_struct, int pi
     struct task_struct *task;
     char * path;
     struct timespec64 current_time;
+    size_t size = 0;
+    int *child_list = NULL;
+
+    if (p_info_struct == NULL) {
+        rcu_read_lock();
+        task = pid_task(find_vpid(pid), PIDTYPE_PID);
+        if (task) {
+            size = list_count_nodes(&(task->children));
+            rcu_read_unlock();
+            return size;
+        }
+        rcu_read_unlock();
+        return 0;
+    }
 
     struct pid_info *data = (struct pid_info *) kzalloc(sizeof(struct pid_info), GFP_KERNEL);
     if (data == NULL) {
         return -ENOMEM;
     }
+    memset(data, 0, sizeof(struct pid_info));
 
     int error = copy_from_user(data, p_info_struct, sizeof(struct pid_info));
     if (error) {
@@ -71,14 +86,46 @@ static long    __sys_get_pid_info(struct pid_info __user * p_info_struct, int pi
         } else {
             memcpy(data->working_directory, path, PATH_MAX);
         }
+        size = list_count_nodes(&(task->children));
+        if (size == 0) {
+            child_list = NULL;
+        } else {
+            child_list = kzalloc(sizeof(int) * size, GFP_KERNEL);
+            if (child_list == NULL){
+                kfree(data);
+                rcu_read_unlock();
+                return -ENOMEM;
+            }
+            struct task_struct *child_task;
+            size_t index = 0;
+            list_for_each_entry(child_task, &task->children, sibling) {
+                child_list[index] = child_task->pid;
+
+                pr_info("%d\n", child_list[index]);
+                index ++;
+            }
+        }
     }
     rcu_read_unlock();
+    if (child_list != NULL) {
+        pr_info("HERE: %d\n", child_list[0]);
+        error = copy_to_user(data->children, child_list, sizeof(int) * size);
+        if (error) {
+            pr_info("HERE?\n");
+            kfree(child_list);
+            kfree(data);
+            return -EFAULT;
+        }
+        kfree(child_list);
+    }
+    pr_info("HERE6\n");
     error = copy_to_user(p_info_struct, data, sizeof(struct pid_info));
     if (error) {
         kfree(data);
         return -EFAULT;
     }
     kfree(data);
+    pr_info("HERE7\n");
     return 0;
 }
 
--- src/tester/main.c
+++ src/tester/main.c
@@ -18,7 +18,7 @@ struct pid_info {
     unsigned int state;
     const void *stack;
     unsigned long long age;
-    // int * children;
+    int *children;
     int parent_pid;
     char root_path[PATH_MAX];
     char working_directory[PATH_MAX];
@@ -29,14 +29,12 @@ long get_pid_info(struct pid_info * ret, int pid) {
     return syscall(__NR_get_pid_info, ret, pid);
 }
 
-int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Usage: %s [pid]\n", argv[0]);
-		return -1;
-	}
+int recurse_get_pid_info(int pid) {
     struct pid_info data;
 
-	long ret = get_pid_info(&data, atoi(argv[1]));
+	long c = get_pid_info(NULL, pid);
+    data.children = malloc(sizeof(int) * c);
+	long ret = get_pid_info(&data, pid);
     if (ret) {
         return ret;
     }
@@ -45,11 +43,30 @@ int main(int argc, char *argv[]) {
     } 
 
     printf("Pid: %d\n", data.pid);
+    printf("# of children: %ld\n", c);
+    printf("Children: [");
+    for(long i = 0; i < c; i++) {
+        printf("%d, ", data.children[i]);
+    }
+    printf("]\n");
     printf("State; %d\n", data.state);
     printf("Age : %lld ms\n", data.age / 1000000);
+    printf("Stack address: %p\n", data.stack);
     printf("Parent PID: %d\n", data.parent_pid);
     printf("Root Path: %s\n", data.root_path);
-    printf("Current Working Directory: %s\n", data.working_directory);
-    
+    printf("Current Working Directory: %s\n\n", data.working_directory);
+    int r = 0;
+    for(long i = 0; i < c; i++) {
+        r += recurse_get_pid_info(data.children[i]);
+    }
+    return r;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Usage: %s [pid]\n", argv[0]);
+		return -1;
+	}
+    return recurse_get_pid_info(atoi(argv[1]));
 }
 
